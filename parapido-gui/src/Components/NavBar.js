17d6bab46496e051e19fcf4f29df70253fb635a7
Stephanie <stephanie.vargas@upr.edu>
2021-10-04 17:11:47 -0400

    added styling to nav bar elements

--- parapido-gui/src/Components/NavBar.js
+++ parapido-gui/src/Components/NavBar.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom"
 import logo from "../Static/Images/Pa_RapidoLogo.png"
+import "../Layouts/NavBar.css"
 
 export class NavBar extends Component {
     render() {
         return (
-            <nav > 
-                <img style={logoStyle} src={logo} alt="Logo" />
-                <ul style={{listStyleType: "none"}}> 
+            <nav className="NavBar"> 
+                <div className="nav">
+                <img className="logostyle" src={logo} alt="Logo" />
+
+                <ul className="nav-links"> 
                 <li>
-                <Link to="/">Main</Link>
+                <Link to="/">Home</Link>
                 </li>           
                 <li>
                 <Link to="/jobdashboard">Job Dashboard</Link>
@@ -21,19 +24,13 @@ export class NavBar extends Component {
                 <Link to="/"> Sign Out </Link>
                 </li>
                 </ul>
+                </div>
+                
             </nav>
         )
     }
 }
 
-const logoStyle = {
-    background: "black", 
-    width: 60,
-    height: 60, 
-    left: 7,
-    top: 5,
-    position: "absolute"
-}
 
 
 export default NavBar;
