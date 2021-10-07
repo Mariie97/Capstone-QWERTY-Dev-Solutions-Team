a46e0193ef7deef2ec29061d1e508b4c7dfca653
Stephanie <stephanie.vargas@upr.edu>
2021-10-07 01:16:11 -0400

    vm froze commit to save changes of top lettering in card before shutdown

--- parapido-gui/src/Components/ProfileCard.js
+++ parapido-gui/src/Components/ProfileCard.js
@@ -2,7 +2,8 @@ import React from 'react'
 import default_picture from '../Static/Images/defaultprofilepicture.svg'
 import  '../Layouts/ProfileCard.css' 
 import DeleteTwoTone from "@material-ui/icons/DeleteForeverTwoTone"
-
+import ThumbsUpDownIcon from '@material-ui/icons/ThumbsUpDown';
+import StarIcon from '@material-ui/icons/Star';
 
 const ProfileCard = (props) => {
 
@@ -12,47 +13,79 @@ const ProfileCard = (props) => {
     const last_name = "Doe"
     const rating_value = "2.91"
     const cancelled_jobs = 120;
+    const account_type = 2;
+
+    let acc_type;
+
+    if(account_type === 1 ){
+        acc_type = "CLIENT";
+    }
+    else if(account_type === 2){
+        acc_type = "STUDENT"
+    }
+    else{
+        acc_type = "ADMIN"
+    }
 
    
 
     return (
+     
+        <React.Fragment>
+        <div className= "top-card-lettering"> {acc_type} </div>
         <div className="body-profile">
-
         <div className = "card">
 
                 <div className = "box-top">
-                    <img className = "profile-image" style={image_resize_margin} src= {default_picture} alt="profile" />  
+                    <img className = "profile-image" style={image_resize} src= {default_picture} alt="profile" />  
                 </div>
 
                 <ul className = "header-list"> 
-                   <li> {user_name} {last_name} </li>
-                   <li> Rating: { rating_value} of 5 </li>            
+                   <li style= {{fontWeight : "bold"}}> {user_name} {last_name} </li>
+                   <li> <StarIcon style = {staricon}/> Rating: { rating_value} of 5 <ThumbsUpDownIcon style = {thumbsupdown}/></li>            
                 </ul>
                 
-                <p> <DeleteTwoTone style={trashcan} /> Jobs Cancelled: {cancelled_jobs} </p> 
-
-        </div>
-
-        </div>
-
+                {/* footer */}
+       
+               
+                <p className="footer-line"> <DeleteTwoTone style={trashcan} /> Jobs Cancelled: {cancelled_jobs} </p> 
+           
     
+        </div>
+
+        </div>
+
+        </React.Fragment>
     )
 }
 
-// small elements css
+// small icons & elements css
 
 const trashcan = {
     color: "red",
     position: "relative",
-    top: 6,
+    top: 7,
 }
 
-const image_resize_margin = {
-    height: 150, 
-    width: 150, 
-    marginTop: 0, 
-    marginBottom: 5
+const thumbsupdown = {
+    color: "burlywood",
+    position: "relative",
+    width: 15,
+    top: 6,
+    left:2
+}
 
+const staricon = {
+    color: "yellow",
+    position: "relative",
+    width: 19,
+    top: 6,
+    left:2
+}
+
+const image_resize = {
+    height: 180, 
+    width: 180,
 }
 
 
