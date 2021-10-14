3d9c26f386d2ed5aecfc6992c5a742abdaac51eb
Stephanie <stephanie.vargas@upr.edu>
2021-10-13 23:14:38 -0400

    finished styling for the left 2 inputs of title and description in the left body container

--- parapido-gui/src/Components/JobCreation.js
+++ parapido-gui/src/Components/JobCreation.js
@@ -14,23 +14,23 @@ export class JobCreation extends Component {
                  <h1 className="job-creation-page-header"> Job Creation </h1>
                 
                 <div className="big-flexbox-for-2-flexbox-containers">
-                    <div>
-                        <h5> hi</h5>
-                        <h5> hi</h5>
+                    <div className="left-body-container-1-job-creation">
+                        <label className="label-job-creation"> *Title: </label>
+                        <input className="input-1-job-creation" type="text" id="title" name="title" placeholder="Title"></input>
+                        <label className="label-job-creation"> *Description: </label>
+                        <textarea className="input-2-job-creation" type="text" id="description" name="description" placeholder="Description"></textarea>
                     </div>
                     <div>
-                        <h5> hi </h5>
-                        <h5> hi </h5>
+                        <h5> street </h5>
+                        <h5> city </h5>
+                        <h5> zipcode </h5>
                     </div>
                   
                 </div>
-
-
-
-
             </React.Fragment>
         )
     }
 }
+
 
 export default JobCreation
