b9be1932abc47cfedcd895a034e3424b9b9b0e85
Stephanie <stephanie.vargas@upr.edu>
2021-10-25 22:45:48 -0400

    need to checkout branch and working on basic layout on job dashboard

--- parapido-gui/src/Components/JobDashboardPage.js
+++ parapido-gui/src/Components/JobDashboardPage.js
@@ -1,5 +1,6 @@
 import {Component} from 'react';
 import {Redirect} from "react-router-dom";
+import "../Layouts/JobDashboard.css"
 import verifyUserAuth from "../Utilities";
 
 
@@ -16,14 +17,29 @@ class JobDashboardPage extends Component {
         this.setState({
             is_auth: verifyUserAuth(this.props.cookies.get('csrf_access_token'))
         });
+
+        // webpage background color
+		document.body.style.backgroundColor = "#2F2D4A";
     }
 
     render() {
         const {is_auth} = this.state;
         return (
-            <div className="Dashboard">
+            <div>
                 {!is_auth && <Redirect to='/' />}
-                <h1>Job Dashboard</h1>
+                <h1 className="job-dashboard-page-header">Job Dashboard</h1>
+                <div className="first-flex-container-job-dashboard-page">
+                    <div>
+                        Category
+                    </div>
+                    <div>
+                        Price
+                    </div>
+                    <div>
+                        City
+                    </div>
+                    <button> Press to FILTER </button>
+                </div>
             </div>
         )
     }
