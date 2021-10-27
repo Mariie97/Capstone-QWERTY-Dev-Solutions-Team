ae4843c14874753955dfb944241df8851e2bc621
Coralys M Cortes Soto <coralys.cortes1@upr.edu>
2021-10-26 22:41:47 -0400

    Fixed App.js

--- parapido-gui/src/App.js
+++ parapido-gui/src/App.js
@@ -5,70 +5,67 @@ import LandingPage from "./Components/LandingPage"
 import ProfilePage from "./Components/ProfilePage";
 import JobDashboardPage from "./Components/JobDashboardPage";
 import NavBar from './Components/NavBar';
+import UserRegistrationPage from "./Components/UserRegistrationPage";
 import JobCreation from "./Components/JobCreation";
-
 
 
 class App extends React.Component {
     render() {
         return (
             <BrowserRouter>
-                <div>
-                    <Switch>
-                        <Route
-                            exact
-                            path='/'
-                            render={() => (
-                                <React.Fragment>
-                                    <LandingPage cookies= {this.props.cookies} />
-                                </React.Fragment>
-                            )}
-                        />
-                        <Route
-                            exact
-                            path='/profile/:user_id'
-                            render={(props) => (
-                                <React.Fragment>
-                                    <NavBar cookies={this.props.cookies} />
-                                    <ProfilePage
-                                        cookies={this.props.cookies}
-                                        user_id={props.match.params.user_id}
-                                    />
-                                </React.Fragment>
-                            )}
-                        />
-                        <Route
-                            exact
-                            path='/jobdashboard'
-                            render={() => (
-                                <React.Fragment>
-                                    <NavBar cookies= {this.props.cookies} />
-                                    <JobDashboardPage cookies= {this.props.cookies}/>
-                                </React.Fragment>
-                            )}
-                        />
-                         <Route
+                <Switch>
+                    <Route
+                        exact
+                        path='/'
+                        render={() => (
+                            <React.Fragment>
+                                <LandingPage cookies={this.props.cookies} />
+                            </React.Fragment>
+                        )}
+                    />
+                    <Route
+                        exact
+                        path='/profile/:user_id'
+                        render={(props) => (
+                            <React.Fragment>
+                                <NavBar cookies={this.props.cookies} />
+                                <ProfilePage
+                                    cookies={this.props.cookies}
+                                    user_id={props.match.params.user_id}
+                                />
+                            </React.Fragment>
+                        )}
+                    />
+                    <Route
+                        exact
+                        path='/jobdashboard'
+                        render={() => (
+                            <React.Fragment>
+                                <NavBar cookies= {this.props.cookies} />
+                                <JobDashboardPage cookies= {this.props.cookies}/>
+                            </React.Fragment>
+                        )}
+                    />
+                    <Route
+                        exact
+                        path='/signup'
+                        render={() => (
+                            <React.Fragment>
+                                <UserRegistrationPage />
+                            </React.Fragment>
+                        )}
+                    />
+                    <Route
                             exact
                             path='/jobcreation'
                             render={() => (
                                 <React.Fragment>
-                                    <NavBar cookies={this.props.cookies}/>
-                                    <JobCreation cookies={this.props.cookies}/>
+                                    <NavBar cookies={this.props.cookies} />
+                                    <JobCreation cookies={this.props.cookies} />
                                </React.Fragment>
                             )}
                         />
-
-                        <Route
-                        exact
-                        path='/signup'
-                        render={() => (
-                        <React.Fragment>
-                        <h1>Register Account</h1>
-                        </React.Fragment>
-                    )}
-                        />
-                    </Switch>
-                </div>
+                </Switch>
             </BrowserRouter>
         );
     }
