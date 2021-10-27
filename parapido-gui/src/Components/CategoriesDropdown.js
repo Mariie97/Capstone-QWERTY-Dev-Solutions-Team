529a4dfa5db80479d8b5625b8aa7ed97f38d80f9
Coralys M Cortes Soto <coralys.cortes1@upr.edu>
2021-10-26 22:40:11 -0400

    -Fixed indentation and cleaned code

--- parapido-gui/src/Components/CategoriesDropdown.js
+++ parapido-gui/src/Components/CategoriesDropdown.js
@@ -1,6 +1,5 @@
-
-import React, { Component } from 'react';
-import {InputLabel, MenuItem, Select} from "@material-ui/core";
+import React, {Component} from 'react';
+import {MenuItem, Select} from "@material-ui/core";
 import StyledEngineProvider from '@material-ui/styles/StylesProvider';
 import ReportProblemIcon from '@material-ui/icons/ReportProblem';
 
@@ -16,18 +15,15 @@ class CategoriesDropdown extends Component {
         };
 
         // event method - before render method
-
         this.handleOnChangeCategory = this.handleOnChangeCategory.bind(this);
-        
-        // validation method - end of render method
 
+        // validation method - end of render method
         this.validateCategory = this.validateCategory.bind(this);
     }
 
     handleOnChangeCategory(event) {
         this.setState({
             category: event.target.value.toString()
-
         });
     }
 
@@ -37,7 +33,6 @@ class CategoriesDropdown extends Component {
         return (
             <StyledEngineProvider injectFirst>
                 <div>
-                    <InputLabel></InputLabel>
                     <Select
                         value={category}
                         onChange={this.handleOnChangeCategory}
@@ -54,9 +49,7 @@ class CategoriesDropdown extends Component {
                             }
                         }}
                         disableUnderline
-                        >
-                    
-
+                    >
                         <MenuItem value={1}>Animals</MenuItem>
                         <MenuItem value={2}>Auto</MenuItem>
                         <MenuItem value={3}>Education</MenuItem>
@@ -64,42 +57,39 @@ class CategoriesDropdown extends Component {
                         <MenuItem value={5}>Home</MenuItem>
                         <MenuItem value={6}>Self-Care</MenuItem>
                         <MenuItem value={7}>Shop</MenuItem>
-                        <MenuItem value={8}>Other</MenuItem>                   
+                        <MenuItem value={8}>Other</MenuItem>
                     </Select>
                     {this.state.categoryError !== undefined &&
-                        <div className="required-field-2-job-creation">
-                            <hr className="category-error-job-creation"></hr>
-                            <hr className="category-1-error-job-creation"></hr>
-                            <hr className="city-error-2-job-creation"></hr>
-                            <hr className="category-3-error-job-creation"></hr> 
-                            <ReportProblemIcon style={report}/> {categoryError} 
-                        </div>
+                    <div className="required-field-2-job-creation">
+                        <hr className="category-error-job-creation" />
+                        <hr className="category-1-error-job-creation" />
+                        <hr className="city-error-2-job-creation" />
+                        <hr className="category-3-error-job-creation" />
+                        <ReportProblemIcon style={report}/> {categoryError}
+                    </div>
                     }
                 </div>
-            </StyledEngineProvider>  
+            </StyledEngineProvider>
         )
     }
 
-    validateCategory(){   
+    validateCategory(){
         if (this.state.category === undefined) {
             this.setState({
-
-                categoryError: "This field is required" 
+                categoryError: "This field is required"
             })
             document.querySelector('.price-miniflex-job-creation').style.cssText = 'margin-left: 66px;';
             return false;
         }
-       
-        this.setState({
 
+        this.setState({
             categoryError: undefined
         })
         return true;
     }
 }
 
-//small icons & elements css 
-
+//small icons & elements css
 const report = {
     color: "red",
     position: "relative",
