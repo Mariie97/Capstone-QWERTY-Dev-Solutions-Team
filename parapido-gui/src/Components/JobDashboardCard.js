b6d6871f765b38d0d9761ff7304a3ce1d3a366a9
Stephanie <stephanie.vargas@upr.edu>
2021-10-28 20:51:19 -0400

    testing grid and cards

--- parapido-gui/src/Components/JobDashboardCard.js
+++ parapido-gui/src/Components/JobDashboardCard.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import '../Layouts/JobDashboardCard.css'
 
 class JobDashboardCard extends Component {
+    componentDidMount(){
+        document.body.style.backgroundColor = "#2F2D4A";
+    }
+
     render() {
         return (
             <div className="card-body-wrapper">
@@ -21,8 +25,25 @@ class JobDashboardCard extends Component {
                             <div className="card-price"> $1000 </div>
                         </div>
                     </div>
-                </div>
+
+                    <div className="card-wrapper-1">
+                        <div className="card-body">
+                            
+                            <h2 className="card-title"> Recoger la sala
+                                <div className= "circle-square"></div>
+                                <div className= "circle-square2"></div>
+                            </h2>
+                    
+                            <h2 className="card-clientName"> Pepe cojones </h2>
+                            <p className="card-location">
+                            Calle Caoba 00718
+                            </p>
+                            <div className="card-price"> $1000000 </div>
+                        </div>
+                    </div>
+                </div>                  
             </div>
+         
         )
     }
 }
