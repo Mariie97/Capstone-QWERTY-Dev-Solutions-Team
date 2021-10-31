85afc417ee257c633ffc1f95e3686899ab9c5105
Stephanie <stephanie.vargas@upr.edu>
2021-10-30 22:40:17 -0400

    added Link tag to cards in order to route

--- parapido-gui/src/Components/JobDashboardCard.js
+++ parapido-gui/src/Components/JobDashboardCard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import '../Layouts/JobDashboardCard.css'
+import '../Layouts/JobDashboardCard.css';
+import {Link} from "react-router-dom";
 
 class JobDashboardCard extends Component {
     componentDidMount(){
@@ -10,20 +11,22 @@ class JobDashboardCard extends Component {
         return (
             <div className="card-body-wrapper">
                 <div className="card-wrapper">
-                    <div className="card-wrapper-1">
-                        <div className="card-body">
-                            <h2 className="card-title">
-                                <div className="breakTitleJobDashboard"> Recoger la sala </div>
-                                <div className= "circle-square"></div>
-                                <div className= "circle-square2"></div>
-                            </h2>
-                            <h2 className="card-clientName"> FeFa Vargas Villarini</h2>
-                            <p className="card-location">
-                            Calle Caoba 00718
-                            </p>
-                            <div className="card-price"> $1000 </div>
+                    <Link to={"/"}  className="none-display-for-link"> 
+                        <div className="card-wrapper-1">
+                            <div className="card-body">
+                                <h2 className="card-title">
+                                    <div className="breakTitleJobDashboard"> Recoger la sala </div>
+                                    <div className= "circle-square"></div>
+                                    <div className= "circle-square2"></div>
+                                </h2>
+                                <h2 className="card-clientName"> FeFa Vargas Villarini</h2>
+                                <p className="card-location">
+                                Calle Caoba 00718
+                                </p>
+                                <div className="card-price"> $1000 </div>
+                            </div>
                         </div>
-                    </div>
+                    </Link>
 
                     <div className="card-wrapper-1">
                         <div className="card-body">
@@ -41,143 +44,6 @@ class JobDashboardCard extends Component {
                             <div className="card-price"> $1000000 </div>
                         </div>
                     </div>
-
-                    <div className="card-wrapper-1">
-                        <div className="card-body">
-                            
-                            <h2 className="card-title"> 
-                                <div className="breakTitleJobDashboard"> Recoger la sala </div>
-                                <div className= "circle-square"></div>
-                                <div className= "circle-square2"></div>
-                            </h2>
-                    
-                            <h2 className="card-clientName"> Pepe cojones </h2>
-                            <p className="card-location">
-                            Calle Caoba 00718
-                            </p>
-                            <div className="card-price"> $1000000 </div>
-                        </div>
-                    </div>
-
-                    <div className="card-wrapper-1">
-                        <div className="card-body">
-                            
-                            <h2 className="card-title"> 
-                                <div className="breakTitleJobDashboard"> Recoger la sala </div>
-                                <div className= "circle-square"></div>
-                                <div className= "circle-square2"></div>
-                            </h2>
-                    
-                            <h2 className="card-clientName"> Pepe cojones </h2>
-                            <p className="card-location">
-                            Calle Caoba 00718
-                            </p>
-                            <div className="card-price"> $1000000 </div>
-                        </div>
-                    </div>
-
-                    <div className="card-wrapper-1">
-                        <div className="card-body">
-                            
-                            <h2 className="card-title"> 
-                                <div className="breakTitleJobDashboard"> Recoger la sala </div>
-                                <div className= "circle-square"></div>
-                                <div className= "circle-square2"></div>
-                            </h2>
-                    
-                            <h2 className="card-clientName"> Pepe cojones </h2>
-                            <p className="card-location">
-                            Calle Caoba 00718
-                            </p>
-                            <div className="card-price"> $1000000 </div>
-                        </div>
-                    </div>
-
-                    <div className="card-wrapper-1">
-                        <div className="card-body">
-                            
-                            <h2 className="card-title"> 
-                                <div className="breakTitleJobDashboard"> Recoger la sala </div>
-                                <div className= "circle-square"></div>
-                                <div className= "circle-square2"></div>
-                            </h2>
-                    
-                            <h2 className="card-clientName"> Pepe cojones </h2>
-                            <p className="card-location">
-                            Calle Caoba 00718
-                            </p>
-                            <div className="card-price"> $1000000 </div>
-                        </div>
-                    </div>
-
-                    <div className="card-wrapper-1">
-                        <div className="card-body">
-                            
-                            <h2 className="card-title"> 
-                                <div className="breakTitleJobDashboard"> Recoger la sala </div>
-                                <div className= "circle-square"></div>
-                                <div className= "circle-square2"></div>
-                            </h2>
-                    
-                            <h2 className="card-clientName"> Pepe cojones </h2>
-                            <p className="card-location">
-                            Calle Caoba 00718
-                            </p>
-                            <div className="card-price"> $1000000 </div>
-                        </div>
-                    </div>
-
-                    <div className="card-wrapper-1">
-                        <div className="card-body">
-                            
-                            <h2 className="card-title"> 
-                                <div className="breakTitleJobDashboard"> Recoger la sala </div>
-                                <div className= "circle-square"></div>
-                                <div className= "circle-square2"></div>
-                            </h2>
-                    
-                            <h2 className="card-clientName"> Pepe cojones </h2>
-                            <p className="card-location">
-                            Calle Caoba 00718
-                            </p>
-                            <div className="card-price"> $1000000 </div>
-                        </div>
-                    </div>
-
-                    <div className="card-wrapper-1">
-                        <div className="card-body">
-                            
-                            <h2 className="card-title"> 
-                                <div className="breakTitleJobDashboard"> Recoger la sala </div>
-                                <div className= "circle-square"></div>
-                                <div className= "circle-square2"></div>
-                            </h2>
-                    
-                            <h2 className="card-clientName"> Pepe cojones </h2>
-                            <p className="card-location">
-                            Calle Caoba 00718
-                            </p>
-                            <div className="card-price"> $1000000 </div>
-                        </div>
-                    </div>
-
-                    <div className="card-wrapper-1">
-                        <div className="card-body">
-                            
-                            <h2 className="card-title"> 
-                                <div className="breakTitleJobDashboard"> Recoger la sala </div>
-                                <div className= "circle-square"></div>
-                                <div className= "circle-square2"></div>
-                            </h2>
-                    
-                            <h2 className="card-clientName"> Pepe cojones </h2>
-                            <p className="card-location">
-                            Calle Caoba 00718
-                            </p>
-                            <div className="card-price"> $1000000 </div>
-                        </div>
-                    </div>
-
                 </div>                  
             </div>
          
