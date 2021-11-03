63615854fe09b75f0507286c6a815a25d86002ab
Stephanie <stephanie.vargas@upr.edu>
2021-11-03 18:32:58 -0400

    added basic styling to the login modal overall

--- parapido-gui/src/Components/AgreementModal.js
+++ parapido-gui/src/Components/AgreementModal.js
@@ -6,6 +6,7 @@ import StyledEngineProvider from '@material-ui/styles/StylesProvider';
 import {Box, Modal} from "@material-ui/core";
 import loginModalLogo from '../Static/Images/Pa_Rapido_logo_bgPalette.png';
 import continueArrow from '../Static/Images/continueArrow.png';
+import virtualContract from '../Static/Images/Virtual_Contract.png';
 
 const style = {
     position: 'absolute',
@@ -17,7 +18,8 @@ const style = {
     border: '3px solid black',
     boxShadow: 24,
     p: 4,
-    padding: '9px'
+    padding: '9px',
+    borderRadius: "1vh"
 }
 
 class AgreementModal extends Component {
@@ -46,7 +48,7 @@ class AgreementModal extends Component {
             <StyledEngineProvider injectFirst>
                 
                     <div>
-                        <div onClick={this.toggleModal}>Open modal</div>
+                        <div onClick={this.toggleModal}>Agreement Modal</div>
                         <Modal
                             open={open}
                             onClose={this.toggleModal}
@@ -60,6 +62,7 @@ class AgreementModal extends Component {
                               
                                 <div className="body-container-agreement-modal">
                                     <p className="long-text-agreement-modal"> Check to state that you have read and agree with the job information </p>
+                                    <img style={virtual_contract_image_resize} src={virtualContract} alt="continue arrow" />
                                     <button onClick={this.handleOnClick} className="agreement-modal-continue-button">
                                         <div className="text-button-login-modal">
                                             Agree & Continue
@@ -89,6 +92,12 @@ const continue_arrow_image_resize = {
     width: 100,
     position: "fixed",
     bottom: 25,
+};
+
+const virtual_contract_image_resize = {
+    height: 80,
+    width: 75,
+    marginBottom: "3vh"
 };
 
 export default AgreementModal;
