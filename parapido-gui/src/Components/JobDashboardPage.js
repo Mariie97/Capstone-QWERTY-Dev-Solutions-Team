028024cae232f7fa855630542d36bcf3939c76ac
Coralys M Cortes Soto <coralys.cortes1@upr.edu>
2021-11-04 20:12:32 -0400

    -Removed console.log() and dead code -Removed increment + 1 in city, the item value in the dropdown no need to increase the count by one.

--- parapido-gui/src/Components/JobDashboardPage.js
+++ parapido-gui/src/Components/JobDashboardPage.js
@@ -10,9 +10,9 @@ import {Box, CircularProgress} from "@material-ui/core";
 
 class JobDashboardPage extends Component {
     currentUser = {
-    id: parseInt(localStorage.getItem('user_id')),
-    type: parseInt(localStorage.getItem('type'))
-};
+        id: parseInt(localStorage.getItem('user_id')),
+        type: parseInt(localStorage.getItem('type'))
+    };
 
     constructor(props) {
         super(props);
@@ -36,33 +36,27 @@ class JobDashboardPage extends Component {
         });
 
         // webpage background color
-		document.body.style.backgroundColor = "#2F2D4A";
+        document.body.style.backgroundColor = "#2F2D4A";
         this.getJobs();
     }
 
-    goToJobCreation(){
-
-    }
     getJobs() {
         fetch("jobs_list/1", {
-          method: 'GET'
+            method: 'GET'
         }).then(response => {
-          if (response.status === 200) {
-            response.json().then(data => { 
-              this.setState(
-                  {jobs : data,
-                  pageLoaded : true,
-                  filterLoaded : true
-                  }
-                  )      
-            })
-           
-          }
-          else{
-              console.log("Error request couldn't get processed")
-          }
+            if (response.status === 200) {
+                response.json().then(data => {
+                    this.setState(
+                        {jobs : data,
+                            pageLoaded : true,
+                            filterLoaded : true
+                        }
+                    )
+                })
+
+            }
         })
-      }
+    }
 
     clickFilter() {
         this.setState(
@@ -71,67 +65,63 @@ class JobDashboardPage extends Component {
 
         let category = this.state.change_category?.current.state.item;
         let city = this.state.change_city?.current.state.item;
-        let selectedCity = 0;
         let filterResult = '?';
         let price = this.state.change_price?.current.state.item;
 
-        if(category !== undefined && category !== '0'){
-            console.log(category)
-            console.log(typeof(category))
+        if(category !== '' && category !== '0'){
             category = `category=${category}`;
             filterResult += category
         }
         if(category === '0'){
             filterResult = '?';
-        }       
+        }
 
-        if(city !== undefined && city !== '0'){
-            selectedCity = +city - 1;
+        if(city !== '' && city !== '0'){
             if(filterResult.length === 1){
-                city = `city=${selectedCity}`;  
+                city = `city=${city}`;
                 filterResult += city
             }
             else{
-                city = `&city=${selectedCity}`; 
+                city = `&city=${city}`;
                 filterResult += city;
             }
         }
 
-        if(price !== undefined && price !== '0'){
+        if(price !== '' && price !== '0'){
             if(filterResult.length === 1){
                 if(price === '1'){
-                    price = `maxPrice=20`;  
+                    price = `maxPrice=20`;
                 }
                 else if(price === '2'){
-                    price = `minPrice=20&maxPrice=40`;  
+                    price = `minPrice=20&maxPrice=40`;
                 }
                 else if(price === '3'){
-                    price = `minPrice=50&maxPrice=60`;  
+                    price = `minPrice=50&maxPrice=60`;
                 }
                 else if(price === '4'){
-                    price = `minPrice=70&maxPrice=100`;  
+                    price = `minPrice=70&maxPrice=100`;
                 }
                 else if(price === '5'){
-                    price = `minPrice=100`;  
+                    price = `minPrice=100`;
                 }
-                
+
                 filterResult += price
             }
             else{
                 if(price === '1'){
-                    price = `&maxPrice=20`;  
+                    price = `&maxPrice=20`;
                 }
                 else if(price === '2'){
-                    price = `&minPrice=20&maxPrice=40`;  
+                    price = `&minPrice=20&maxPrice=40`;
                 }
                 else if(price === '3'){
-                    price = `&minPrice=50&maxPrice=60`;  
+                    price = `&minPrice=50&maxPrice=60`;
                 }
                 else if(price === '4'){
-                    price = `&minPrice=70&maxPrice=100`;  
+                    price = `&minPrice=70&maxPrice=100`;
                 }
                 else if(price === '5'){
-                    price = `&minPrice=100`;  
+                    price = `&minPrice=100`;
                 }
 
                 filterResult += price
@@ -140,44 +130,43 @@ class JobDashboardPage extends Component {
 
         fetch(`jobs_list/1`+filterResult, {
             method: 'GET'
-          }).then(response => {
+        }).then(response => {
             if(category=== '0' && cities === '0' && price === '0'){
                 this.getJobs();
             }
             else if (response.status === 200) {
-              response.json().then(data => { 
-                this.setState(
-                    {jobs : data,
-                     filterLoaded : true
-                    }
-                    ) 
-              }) 
+                response.json().then(data => {
+                    this.setState(
+                        {jobs : data,
+                            filterLoaded : true
+                        }
+                    )
+                })
             }
             else{
                 this.setState(
                     {jobs : [],
-                     filterLoaded: true}
-                ) 
-                console.log("There are no jobs with for this filter.")
+                        filterLoaded: true}
+                )
             }
-          })
+        })
 
     }
 
     render() {
         const { jobs, change_category, change_city, change_price, is_auth, pageLoaded, filterLoaded } = this.state;
         const showJobCreationButton = this.currentUser.type === accountType.client;
-        const cardArray = jobs.map( 
-            job => <JobDashboardCard 
-            job_id = {job.job_id}
-            title = {job.title} 
-            city = {job.city}
-            price = {job.price}
-            category = {job.categories}
-            owner_first  = {job.owner_first}
-            owner_last = {job.owner_last}
-            street = {job.street}
-            zipcode = {job.zipcode}
+        const cardArray = jobs.map(
+            job => <JobDashboardCard
+                job_id = {job.job_id}
+                title = {job.title}
+                city = {job.city}
+                price = {job.price}
+                category = {job.categories}
+                owner_first  = {job.owner_first}
+                owner_last = {job.owner_last}
+                street = {job.street}
+                zipcode = {job.zipcode}
             />
         )
 
@@ -192,63 +181,60 @@ class JobDashboardPage extends Component {
                         </Box>
                     </div> :
                     <div>
-                    <h1 className="job-dashboard-page-header">Job Dashboard
-                        {showJobCreationButton &&
-                        <Link to="/jobcreation">
-                        <button className="create-job-button-job-dashboard">
-                            <div className="text1-button-job-dashboard">
-                            Create Job
+                        <h1 className="job-dashboard-page-header">Job Dashboard
+                            {showJobCreationButton &&
+                            <Link to="/jobcreation">
+                                <button className="create-job-button-job-dashboard">
+                                    <div className="text1-button-job-dashboard">
+                                        Create Job
+                                    </div>
+                                </button>
+                            </Link>
+                            }
+                        </h1>
+
+                        <div className="first-flex-container-job-dashboard-page">
+                            <div className = "label-job-dashboard">
+                                <ItemsDropdown
+                                    ref={change_category}
+                                    itemsList={categories}
+                                    label='Categories'
+                                />
                             </div>
-                        </button>
-                        </Link>
-                        }
-                    </h1>
-                  
-                    <div className="first-flex-container-job-dashboard-page">
-                        <div className = "label-job-dashboard">
-                            <ItemsDropdown
-                                 initial_value={''}
-                                 ref={change_category}
-                                 itemsList={categories}
-                                 label='Categories'
-                            />
-                        </div>
 
-                        <div className = "label-job-dashboard">
-                            <ItemsDropdown
-                                 initial_value={''}
-                                 ref={change_price}
-                                 itemsList={prices}
-                                 label='Prices'
-                            />
-                        </div>
-
-                        <div className = "label-job-dashboard">
-                            <ItemsDropdown
-                                 initial_value={''}
-                                 ref={change_city}
-                                 itemsList={cities}
-                                 label='Cities'
-                            />
-                        </div>
-
-                     
-                        <button className="filter-button-job-dashboard" onClick={this.clickFilter}> 
-                            <div className="text-button-job-dashboard">
-                                <FilterListIcon/>Filter
+                            <div className = "label-job-dashboard">
+                                <ItemsDropdown
+                                    ref={change_price}
+                                    itemsList={prices}
+                                    label='Prices'
+                                />
                             </div>
-                        </button>
-                    </div>
-                    {!filterLoaded ?
-                    <div className='loading-icon1'>
-                        <Box sx={{display: 'flex'}}>
-                            <CircularProgress style={{alignItems:"center"}}/>
-                        </Box>
-                </div> :
-                <div className="card-wrapper">
-                    {cardArray}
-                </div>}
-                </div>}
+
+                            <div className = "label-job-dashboard">
+                                <ItemsDropdown
+                                    ref={change_city}
+                                    itemsList={cities}
+                                    label='Cities'
+                                />
+                            </div>
+
+
+                            <button className="filter-button-job-dashboard" onClick={this.clickFilter}>
+                                <div className="text-button-job-dashboard">
+                                    <FilterListIcon/>Filter
+                                </div>
+                            </button>
+                        </div>
+                        {!filterLoaded ?
+                            <div className='loading-icon1'>
+                                <Box sx={{display: 'flex'}}>
+                                    <CircularProgress style={{alignItems:"center"}}/>
+                                </Box>
+                            </div> :
+                            <div className="card-wrapper">
+                                {cardArray}
+                            </div>}
+                    </div>}
             </div>
 
         )
