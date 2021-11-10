b67e08fc30d15a218cec35b34689c06c28fdd293
Coralys M Cortes Soto <coralys.cortes1@upr.edu>
2021-11-09 20:23:43 -0400

    Fix query params for users and job fetch on AdministrationPage.js

--- parapido-gui/src/Components/AdministrationPage.js
+++ parapido-gui/src/Components/AdministrationPage.js
@@ -1,6 +1,6 @@
 import React, {Component, createRef} from 'react';
 import '../Layouts/AdministrationPage.css'
-import {buildURL, categories, getJobStatus, mapAccount, verifyUserAuth} from "../Utilities";
+import {categories, getJobStatus, mapAccount, verifyUserAuth} from "../Utilities";
 import {Link, Redirect} from "react-router-dom";
 import ItemsDropdown from "./ItemsDropdown";
 import {Box, CircularProgress} from "@material-ui/core";
@@ -35,6 +35,7 @@ class AdministrationPage extends Component {
     getAllUsers() {
         const { deletedRef, typeRef } = this.state;
         let filters = {};
+        let queryParams = '';
 
         if (typeRef.current?.state.item !== undefined && typeRef.current?.state.item !== '') {
             filters.account_type = typeRef.current?.state.item;
@@ -43,8 +44,11 @@ class AdministrationPage extends Component {
             filters.deleted = 'true';
         }
 
-        const url = buildURL('/users', filters);
-        fetch(url, {
+        if (Object.keys(filters).length>0) {
+            queryParams = `?${new URLSearchParams(filters)}`;
+        }
+
+        fetch(`/users/${queryParams}`, {
             method: "GET",
             credentials: 'same-origin',
             headers: {
@@ -76,14 +80,17 @@ class AdministrationPage extends Component {
     getAllJobs() {
         const { jobStatusRef, jobCategoryRef } = this.state;
         const jobStatus = jobStatusRef.current?.state.item !== undefined ? jobStatusRef.current?.state.item : 1;
-
         let filters = {};
+        let queryParams = '';
+
         if (jobCategoryRef.current?.state.item !== undefined && jobCategoryRef.current?.state.item !== '') {
             filters.category = jobCategoryRef.current?.state.item;
         }
+        if (Object.keys(filters).length>0) {
+            queryParams = `?${new URLSearchParams(filters)}`;
+        }
 
-        const url = buildURL(`/jobs_list/${jobStatus}`, filters);
-        fetch(url, {
+        fetch(`/jobs_list/${jobStatus+queryParams}`, {
             method: "GET",
             credentials: 'same-origin',
             headers: {
@@ -238,19 +245,19 @@ class AdministrationPage extends Component {
                             }
                             <div className="admin-filter-button-container">
                                 <button
-                                className='custom-buttons filter admin-filter-button'
-                                onClick={() => {
-                                    this.setState({entitiesLoaded: false});
-                                    if (currentEntity === this.entity.users)
-                                        this.getAllUsers();
-                                    else
-                                        this.getAllJobs();
-                                }}>Filter
-                            </button>
-                            <button
-                                className='custom-buttons filter admin-filter-button'
-                                onClick={this.clearFilters}>Clear Filters
-                            </button>
+                                    className='custom-buttons filter admin-filter-button'
+                                    onClick={() => {
+                                        this.setState({entitiesLoaded: false});
+                                        if (currentEntity === this.entity.users)
+                                            this.getAllUsers();
+                                        else
+                                            this.getAllJobs();
+                                    }}>Filter
+                                </button>
+                                <button
+                                    className='custom-buttons filter admin-filter-button'
+                                    onClick={this.clearFilters}>Clear Filters
+                                </button>
                             </div>
 
                         </div>
