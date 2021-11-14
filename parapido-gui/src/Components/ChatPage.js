83dcd979e29cdee2af5e872254aceb5780288b19
Coralys M Cortes Soto <coralys.cortes1@upr.edu>
2021-11-13 23:40:27 -0400

    Solved problem in chat page that was not sending user's message

--- parapido-gui/src/Components/ChatPage.js
+++ parapido-gui/src/Components/ChatPage.js
@@ -1,6 +1,6 @@
 import React, {Component, createRef} from 'react';
 import '../Layouts/ChatPage.css';
-import {CircularProgress} from "@material-ui/core";
+import {Box, CircularProgress} from "@material-ui/core";
 import Avatar from '@material-ui/core/Avatar';
 import {getQueryParams, verifyUserAuth} from "../Utilities";
 import {Link, Redirect} from "react-router-dom";
@@ -36,8 +36,11 @@ class MessagesContainer extends Component{
   }
 
   addMessages(){
-    return this.props.messages.map((message) => (
-        <li className={`message ${message.sender_id===this.currentUser.id? "right": "left"} appeared`}>
+    return this.props.messages.map((message, index) => (
+        <li
+            key={`message-${index}`}
+            className={`message ${message.sender_id===this.currentUser.id? "right": "left"} appeared`}
+        >
           {message.sender_id!==this.currentUser.id &&
           <Link to={`profile/${message.sender_id}`}>
             <Avatar
@@ -67,7 +70,8 @@ class MessagesContainer extends Component{
 
 class ChatApp extends Component {
   queryParams = undefined;
-  receiver_id = undefined;
+  receiverId = undefined;
+  job_title = undefined;
   currentUser = {
     id: parseInt(localStorage.getItem('user_id')),
     type: parseInt(localStorage.getItem('type'))
@@ -81,12 +85,15 @@ class ChatApp extends Component {
       messages: [],
       current_message: '',
       refreshComplete: true,
+      allowAccess: true,
+      pageLoaded: false,
     };
 
     this.handleKeyPress = this.handleKeyPress.bind(this);
     this.onChange = this.onChange.bind(this);
     this.addMessageBox = this.addMessageBox.bind(this);
     this.getChatMessages = this.getChatMessages.bind(this);
+    this.verifyUserAccess = this.verifyUserAccess.bind(this);
   }
 
   getChatMessages() {
@@ -101,18 +108,39 @@ class ChatApp extends Component {
           this.setState({
             messages: data,
             refreshComplete: true,
+            pageLoaded: true,
           });
-          this.receiver_id = data[0].sender_id===this.currentUser.id ? data[0].receiver_id : data[0].sender_id;
         })
       }
     })
+  }
 
+  async verifyUserAccess() {
+    let data = undefined;
+    const response = await fetch(`/job_details/${this.queryParams.get('job_id')}`, {
+      method:'GET',
+      headers: {
+        'X-CSRF-TOKEN': this.props.cookies.get('csrf_access_token')
+      }
+    })
+    if (response.status === 200) {
+      data = await response.json();
+      if (this.currentUser.id === data.owner_id || this.currentUser.id === data.student_id) {
+        this.receiverId = this.currentUser.id === data.owner_id ? data.student_id : data.owner_id;
+        this.job_title = data.title;
+        this.forceUpdate();
+      }
+      else {
+        this.setState({allowAccess: false});
+      }
+    }
   }
 
   componentDidMount() {
     this.setState({
       is_auth: verifyUserAuth(this.props.cookies.get('csrf_access_token'))
     });
+    this.verifyUserAccess();
     this.getChatMessages();
   }
 
@@ -123,7 +151,7 @@ class ChatApp extends Component {
       sender_id: this.currentUser.id,
     };
 
-    if( current_message && enter){
+    if (current_message && enter){
       fetch("/add_message", {
         method: 'POST',
         credentials: 'same-origin',
@@ -134,7 +162,7 @@ class ChatApp extends Component {
         body: JSON.stringify({
           job_id: this.queryParams.get('job_id'),
           sender_id: this.currentUser.id,
-          receiver_id: this.receiver_id,
+          receiver_id: this.receiverId,
           content: current_message
         })
       }).then(response => {
@@ -147,70 +175,74 @@ class ChatApp extends Component {
   }
 
   onChange(event) {
-    this.setState({
-      current_message: event.target.value
-    });
+    this.setState({current_message: event.target.value});
   }
 
   handleKeyPress(event) {
     let enter_pressed = false;
-    if(event.key === "Enter"){
+    if (event.key === "Enter"){
       enter_pressed = true;
     }
     this.addMessageBox(enter_pressed);
   }
 
   render() {
-    const { is_auth, messages, current_message, refreshComplete } = this.state;
-
-    const access_allowed = this.queryParams.get('student_id') === this.currentUser.id.toString() ||
-        this.queryParams.get('owner_id') === this.currentUser.id.toString();
+    const { is_auth, messages, current_message, refreshComplete, allowAccess, pageLoaded } = this.state;
 
     return (
         <div className='parent-container'>
-          {!is_auth && <Redirect to='/' />}
-          {!access_allowed && <Redirect to='/' /> }
-          <div className='header-flex-container'>
-            <div className="button-flex-container">
-              <Link to={`/job/${this.queryParams.get('job_id')}`}
-                    className='custom-buttons'
-                    id="view-job-button">
-                View Job
-              </Link>
-            </div>
-            <h1 className="page-title-header">Chat: {this.queryParams.get('title')} </h1>
-          </div>
-          <div className='chat-flex-container'>
-            <div className="chat-window">
-              <MessagesContainer messages={messages} />
-              <div className="bottom_wrapper clearfix">
-                <div className="message_input_wrapper">
-                  <input
-                      id="msg_input"
-                      className="message_input"
-                      placeholder="Type your messages here..."
-                      value={current_message}
-                      onChange={this.onChange}
-                      onKeyPress={this.handleKeyPress}/>
+          {(!is_auth || !allowAccess) && <Redirect to='/' />}
+          {!pageLoaded ?
+               <div className='loading-icon'>
+                        <Box sx={{display: 'flex'}}>
+                            <CircularProgress />
+                        </Box>
+                    </div>
+              :
+              <React.Fragment>
+                <div className='header-flex-container'>
+                  <div className="button-flex-container">
+                    <Link to={`/job/${this.queryParams.get('job_id')}`}
+                          className='custom-buttons'
+                          id="view-job-button">
+                      View Job
+                    </Link>
+                  </div>
+                  <h1 className="page-title-header">Chat: {this.job_title} </h1>
                 </div>
-                <button className="send_message" onClick={this.addMessageBox}>Send</button>
-              </div>
-            </div>
-            <div className='refresh-message-container'>
-              {!refreshComplete ?
-                  <div>
-                    <CircularProgress style={{fill: 'white'}}/>
-                  </div> :
-                  <RefreshIcon
-                      className='refresh-msg-icon'
-                      onClick={() => {
-                        this.setState({refreshComplete: false});
-                        this.getChatMessages();
-                      }}
-                  />
-              }
-            </div>
-          </div>
+                <div className='chat-flex-container'>
+                  <div className="chat-window">
+                    <MessagesContainer messages={messages} />
+                    <div className="bottom_wrapper clearfix">
+                      <div className="message_input_wrapper">
+                        <input
+                            id="msg_input"
+                            className="message_input"
+                            placeholder="Type your messages here..."
+                            value={current_message}
+                            onChange={this.onChange}
+                            onKeyPress={this.handleKeyPress}/>
+                      </div>
+                      <button className="send_message" onClick={this.addMessageBox}>Send</button>
+                    </div>
+                  </div>
+                  <div className='refresh-message-container'>
+                    {!refreshComplete ?
+                        <div>
+                          <CircularProgress style={{fill: 'white'}}/>
+                        </div> :
+                        <RefreshIcon
+                            className='refresh-msg-icon'
+                            onClick={() => {
+                              this.setState({refreshComplete: false});
+                              this.getChatMessages();
+                            }}
+                        />
+                    }
+                  </div>
+                </div>
+              </React.Fragment>
+          }
         </div>
     );
   }
